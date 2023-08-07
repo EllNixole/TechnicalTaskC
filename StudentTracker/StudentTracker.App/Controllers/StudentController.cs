fd83ee5498d69a840807bf998cb4f85c6b671c62
Stephanie Riley <stephanie.r@mrsdowsons.co.uk>
2023-08-07 10:09:09 +0100

    Created student deserialiser class and updated views to reflect. Debugged, realised model structure should be amended

--- StudentTracker/StudentTracker.App/Controllers/StudentController.cs
+++ StudentTracker/StudentTracker.App/Controllers/StudentController.cs
@@ -1,12 +1,21 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using StudentTracker.App.Data;
+using StudentTracker.App.Models;
 
 namespace StudentTracker.App.Controllers
 {
     public class StudentController : Controller
     {
+        private List<Student> _studentList;
+
+        public StudentController()
+        {
+            _studentList = StudentDeserialiser.GetStudentList("Data/students.json"); 
+        }
+        
         public IActionResult Index()
         {
-            return View();
+            return View(_studentList);
         }
     }
 }
---
+++ StudentTracker/StudentTracker.App/Data/StudentDeserialiser.cs
@@ -0,0 +1,25 @@
+﻿using StudentTracker.App.Models;
+using System.Text.Json;
+
+namespace StudentTracker.App.Data
+{
+    public class StudentDeserialiser
+    {
+
+        public static List<Student> GetStudentList(string filePath)
+        {
+            try
+            {
+                string jsonText = File.ReadAllText(filePath);
+                List<Student> students = JsonSerializer.Deserialize<List<Student>>(jsonText);
+                return students;
+            }
+            catch (Exception e) 
+            {
+                Console.WriteLine("Error during deserialisation: " + e.Message);
+                return null;
+            }
+        }
+
+    }
+}
