c3d8c0365ac1b9fdec549a434d7f5448243ce2e0
Stephanie Riley <stephanie.r@mrsdowsons.co.uk>
2023-08-07 09:00:03 +0100

    Created a Student model representing the data

---
+++ StudentTracker/StudentTracker.App/Models/Student.cs
@@ -0,0 +1,19 @@
+﻿namespace StudentTracker.App.Models
+{
+    public class Student
+    {
+
+        public string Name { get; set; }
+
+        public String University { get; set; }
+
+        public string Major { get; set; }
+
+        public int Id { get; set; }
+
+        public Dictionary<string, double> Grades { get; set; }
+
+        public Dictionary<string, double> Attendance { get; set; }
+
+    }
+}
